4c6202d2af669733a68ee4b8e47283b5334af941
Alex Wang <36569033@qq.com>
2019-03-25 15:40:48 +0800

    Fix compile error

--- selfdrive/locationd/parse_ublox_desc.py
+++ selfdrive/locationd/parse_ublox_desc.py
@@ -1,4 +1,5 @@
 from ublox import *
+import struct
 
 format_to_c_type = {
   'c': 'int8_t',
@@ -17,6 +18,39 @@ format_to_c_type = {
   'd': 'double',
 }
 
+def print_c_defs(msg_format, fields):
+  formats = msg_format[1:].split(',')
+  for fmt in formats:
+    j = 0
+    while j < len(fmt):
+      f = fmt[j]
+      j += 1
+      if '0' <= f <= '9':
+        num = ord(f) - ord('0')
+        while(j < len(fmt)) and '0' <= fmt[j] <= '9':
+          num = num * 10 + (ord(fmt[j]) - ord('0'))
+          j += 1
+        if j >= len(fmt):
+          break
+        f = fmt[j]
+        j += 1
+      else:
+        num = 1
+      #print(num, f)
+      if num == 1:
+        field = fields.pop(0)
+        print('  {} {};'.format(format_to_c_type[f], field))
+      else:
+        field = fields.pop(0)
+        (_, alen) = ArrayParse(field)
+        if alen == -1:
+          print('  {} {};'.format(format_to_c_type[f], field))
+          for _ in range(num - 1):
+            field = desc.fields.pop(0)
+            print('  {} {};'.format(format_to_c_type[f], field))
+        else:
+          print('  {} {};'.format(format_to_c_type[f], field))
+
 if __name__ == "__main__":
   a = [(CLASS_NAV, MSG_NAV_PVT), (CLASS_RXM, MSG_RXM_RAW), (CLASS_RXM, MSG_RXM_SFRBX)]
   for t in a:
@@ -28,34 +62,16 @@ if __name__ == "__main__":
     print('format2', desc.format2)
     print('fields2', desc.fields2)
 
-    formats = desc.msg_format[1:].split(',')
-    for fmt in formats:
-      j = 0
-      while j < len(fmt):
-        f = fmt[j]
-        j += 1
-        if '0' <= f <= '9':
-          num = ord(f) - ord('0')
-          while(j < len(fmt)) and '0' <= fmt[j] <= '9':
-            num = num * 10 + (ord(fmt[j]) - ord('0'))
-            j += 1
-          if j >= len(fmt):
-            break
-          f = fmt[j]
-          j += 1
-        else:
-          num = 1
-        print(num, f)
-        if num == 1:
-          field = desc.fields.pop(0)
-          print('{} {};'.format(format_to_c_type[f], field))
-        else:
-          field = desc.fields.pop(0)
-          (_, alen) = ArrayParse(field)
-          if alen == -1:
-            print('{} {};'.format(format_to_c_type[f], field))
-            for _ in range(num - 1):
-              field = desc.fields.pop(0)
-              print('{} {};'.format(format_to_c_type[f], field))
-          else:
-            print('{} {};'.format(format_to_c_type[f], field))
+    print("struct {}_msg {{".format(desc.name.lower()))
+    print_c_defs(desc.msg_format, desc.fields)
+    print("};")
+    if desc.format2:
+      print('optional data size is multiple of {}'.format(struct.calcsize(desc.format2)))
+      if not desc.count_field:
+        print('No optional data')
+      elif desc.count_field == '_remaining':
+        print('Optional data count is auto caulated')
+      else:
+        print('Optional data count is in {}'.format(desc.count_field))
+      print_c_defs(desc.format2, desc.fields2)
+
