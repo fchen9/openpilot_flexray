f62bbb47552842baa4735c447831c1656c2b5e54
Alex Wang <36569033@qq.com>
2019-03-25 13:03:48 +0800

    Fix compile error

--- selfdrive/locationd/ubloxd.cc
+++ selfdrive/locationd/ubloxd.cc
@@ -9,10 +9,6 @@
 #include <sys/cdefs.h>
 #include <sys/types.h>
 #include <sys/time.h>
-#include <sys/socket.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <assert.h>
 #include <algorithm>
 
@@ -33,9 +29,19 @@ namespace {
 const uint8_t PREAMBLE1 = 0xb5;
 const uint8_t PREAMBLE2 = 0x62;
 
+// message classes
+const uint8_t CLASS_NAV = 0x01;
+const uint8_t CLASS_RXM = 0x02;
+
+// NAV messages
+const uint8_t MSG_NAV_PVT = 0x7;
+
+// RXM messages
+const uint8_t MSG_RXM_RAW = 0x15;
+
 volatile int do_exit = 0; // Flag for process exit on signal
 const long ZMQ_POLL_TIMEOUT = 1000; // In miliseconds
-const int UBLOX_HEADER_SIZE = 8;
+const int UBLOX_HEADER_SIZE = 6;
 const int UBLOX_MAX_MSG_SIZE = 65536;
 uint8_t msg_parse_buf[UBLOX_HEADER_SIZE + UBLOX_MAX_MSG_SIZE];
 size_t bytes_in_parse_buf = 0U;
@@ -43,21 +49,21 @@ size_t bytes_in_parse_buf = 0U;
 inline int needed_bytes() {
 	if(bytes_in_parse_buf < UBLOX_HEADER_SIZE)
 		return UBLOX_HEADER_SIZE - bytes_in_parse_buf;
-	return UBLOX_MSG_SIZE(msg_parse_buf) + UBLOX_HEADER_SIZE - bytes_in_parse_buf;
+	return UBLOX_MSG_SIZE(msg_parse_buf) + UBLOX_HEADER_SIZE + 2 - bytes_in_parse_buf;
 }
 
 inline bool valid_cheksum() {
 	uint8_t ck_a = 0, ck_b = 0;
-	for(int i = 2; i <= 5;i++) {
+	for(int i = 2; i < bytes_in_parse_buf - 2;i++) {
 		ck_a = (ck_a + msg_parse_buf[i]) & 0xFF;
 		ck_b = (ck_b + ck_a) & 0xFF;
 	}
-	if(ck_a != msg_parse_buf[6]) {
-		LOGD("Checksum a mismtach: %u, %u", ck_a, msg_parse_buf[6]);
+	if(ck_a != msg_parse_buf[bytes_in_parse_buf - 2]) {
+		LOGD("Checksum a mismtach: %02X, %02X", ck_a, msg_parse_buf[6]);
 		return false;
 	}
-	if(ck_b != msg_parse_buf[7]) {
-		LOGD("Checksum b mismtach: %u, %u", ck_b, msg_parse_buf[7]);
+	if(ck_b != msg_parse_buf[bytes_in_parse_buf - 1]) {
+		LOGD("Checksum b mismtach: %02X, %02X", ck_b, msg_parse_buf[7]);
 		return false;
 	}
 	return true;
@@ -69,11 +75,12 @@ inline bool valid() {
 
 bool valid_so_far() {
 	if(bytes_in_parse_buf > 0 && msg_parse_buf[0] != PREAMBLE1) {
-		LOGD("PREAMBLE1 invalid, %u.", msg_parse_buf[0]);
+		LOGD("PREAMBLE1 invalid, %02X.", msg_parse_buf[0]);
+		std::exit(-1);
 		return false;
 	}
 	if(bytes_in_parse_buf > 1 && msg_parse_buf[1] != PREAMBLE2) {
-		LOGD("PREAMBLE2 invalid, %u.", msg_parse_buf[1]);
+		LOGD("PREAMBLE2 invalid, %02X.", msg_parse_buf[1]);
 		return false;
 	}
 	if(needed_bytes() == 0 && !valid())
@@ -95,7 +102,7 @@ size_t msg_parser_handle_data(const uint8_t *incoming_data, uint16_t incoming_da
 	if(bytes_in_parse_buf < UBLOX_HEADER_SIZE)
 		return bytes_consumed;
 	LOGD("ublox msg size: %u", UBLOX_MSG_SIZE(msg_parse_buf));
-	if(bytes_in_parse_buf == UBLOX_MSG_SIZE(msg_parse_buf) + UBLOX_HEADER_SIZE) {
+	if(bytes_in_parse_buf == UBLOX_MSG_SIZE(msg_parse_buf) + UBLOX_HEADER_SIZE + 2) {
 		LOGD("ublox msg total size: %u", bytes_in_parse_buf);
 		#if 0
 		capnp::MallocMessageBuilder msg;
@@ -165,7 +172,6 @@ int main() {
   LOGW("starting ubloxd");
 	signal(SIGINT, (sighandler_t) set_do_exit);
   signal(SIGTERM, (sighandler_t) set_do_exit);
-  LOG("setpriority returns %d", err);
   void *context = zmq_ctx_new();
   void *publisher = zmq_socket(context, ZMQ_PUB);
   // flexRay = 8066
